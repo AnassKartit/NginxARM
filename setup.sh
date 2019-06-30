8414f0ea0fc6e36d3810c1aea5e95055fb15e5a3
Anass Kartit <anassk@gmail.com>
2019-06-30 22:45:19 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 #setup
-yum -y install update
+sudo -s
+yum install update
 yum -y update
-yum -y install epel-release
+yum install epel-release
 
 
 # install nginx, update config file
-yum -y install nginx
+sudo yum install nginx
 #sudo wget <config file path> -O /etc/nginx
 systemctl start nginx
 systemctl enable nginx
 systemctl status nginx
+
+
